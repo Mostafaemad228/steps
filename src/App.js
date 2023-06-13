ac71fdc1d31c6e2725afb670afb9d53876d968ae
Mostafaemad228 <42499279+Mostafaemad228@users.noreply.github.com>
2023-06-13 19:10:14 +0200

    add main design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-
+import React, { Fragment } from "react";
 function App() {
   return (
-    <div className="App">
-      <p> Hello World </p>
-    </div>
+    <React.Fragment>
+      <div className="steps">
+        <div className="numbers">
+          <div> 1 </div>
+          <div> 2 </div>
+          <div> 3 </div>
+        </div>
+        <p className="message"> `Hello man` </p>
+        <div className="buttons">
+          <button > Previous </button>
+          <button > Next </button>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
