000f1c02323ea0d313fb19de4fbbd41fe1c62309
Mostafaemad228 <42499279+Mostafaemad228@users.noreply.github.com>
2023-06-14 01:18:56 +0200

    add state for step logic and on click functions to handle the next and previous events

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+
+const Advices = [
+  "Study Hard",
+  "Applay For job",
+  "Enjoy your money",
+]
+
 function App() {
+  const [steps , setSteps] = useState(1)
+
+  function handllerPrevious() {
+    if (steps > 1)  setSteps(steps - 1)
+  }
+
+  function handllerNext() {
+    if (steps < 3) setSteps(steps + 1)
+  }
+
   return (
     <React.Fragment>
       <div className="steps">
         <div className="numbers">
-          <div> 1 </div>
-          <div> 2 </div>
-          <div> 3 </div>
+          <div className={`${steps >= 1 ? "active": ""}`} > 1 </div>
+          <div className={`${steps >= 2 ? "active": ""}`} > 2 </div>
+          <div className={`${steps >= 3 ? "active": ""}`} > 3 </div>
         </div>
-        <p className="message"> `Hello man` </p>
+        <p className="message"> Step{steps} : {Advices[steps - 1]} </p>
         <div className="buttons">
-          <button > Previous </button>
-          <button > Next </button>
+          <button onClick={handllerPrevious} > Previous </button>
+          <button onClick={handllerNext} > Next </button>
         </div>
       </div>
     </React.Fragment>
